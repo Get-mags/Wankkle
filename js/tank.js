2785fec0267a45f089cc61833405c4be65475014
laparda14 <120476892+laparda14@users.noreply.github.com>
2024-02-13 09:33:36 -0500

    Update tank.js

--- js/tank.js
+++ js/tank.js
@@ -226,7 +226,7 @@ class Tank {
 		this.centerX = this.x + this.width / 2;
 		this.centerY = this.y + this.height / 2;
 		this.speed = 150;
-		this.rotationSpeed = 25;
+		this.rotationSpeed = rotationSpeed;
 		this.angle = angle;
 		this.turretAngle = turretAngle;
 		this.color = color;
